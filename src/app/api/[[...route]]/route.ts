cbc42b5ec50c2df105c3a5c1b3d0e8590983bc3e
nitinkumarmaurya890 <nitinkumarmaruya890@gmail.com>
2024-12-22 23:16:19 +0530

    Setting up HONO api

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api");
+
+app.get("/project/:projectId", (c) => {
+  const { projectId } = c.req.param();
+
+  return c.json({ projectId: projectId });
+});
+
+export const GET = handle(app);
