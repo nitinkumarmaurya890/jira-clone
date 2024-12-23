35bcc7ad0f96c2522401a6d2df4e03ee3771b573
nitinkumarmaurya890 <nitinkumarmaruya890@gmail.com>
2024-12-23 23:51:22 +0530

    creating auth api -1

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,9 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import auth from "@features/auth/server/route";
 
 const app = new Hono().basePath("/api");
 
-app.get("/project/:projectId", (c) => {
-  const { projectId } = c.req.param();
-
-  return c.json({ projectId: projectId });
-});
+const routes = app.route("/auth", auth);
 
 export const GET = handle(app);
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -16,22 +16,20 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
+import { loginSchema } from "../schemas";
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(1, "Required"),
-});
+
 
 export const SignInCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
     console.log({ values });
   };
   return (
